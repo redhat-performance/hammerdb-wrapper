305c2c73b65872ec1f0a2f48bcd457c4a5b7d761
David Valin <dvalin@redhat.com>
2026-07-27 13:01:56 -0400

    Code review updates

--- hammerdb/hammerdb_scripts/mssql/run_mssql_tpcc.sh
+++ hammerdb/hammerdb_scripts/mssql/run_mssql_tpcc.sh
@@ -67,10 +67,6 @@ then
 benchmark_run_dir=`pwd`"/results"
 fi
 
-echo ${storagetype}
-echo ${Usercount}
-echo ${benchmark_results_dir}
-echo ${whc}
 
 ## Set variables for the run
 
--- hammerdb/hammerdb_scripts/postgres/run_pg_tpcc.sh
+++ hammerdb/hammerdb_scripts/postgres/run_pg_tpcc.sh
@@ -109,7 +109,7 @@ do
     tpm=`grep TPM test_pg_${Testname}_${uc}.out | awk '{print $7}'`
     echo "\"${starttime}\",\"${endtime}\",\"${hostnm}\",\"${krel}\",\"Postgres\",\"${pgver}\",\"${numcpu}\",\"${totmem}\",\"${storagetype}\",\"${uc}\",\"${tpm}\"" >> user-benchmark-result.csv
     cp user-benchmark-result.csv $benchmark_results_dir
-    cp /var/lib//pgsql/data/postgresql.conf $benchmark_results_dir
+    cp /etc/my.cnf $benchmark_results_dir
     mv test_pg_${Testname}_${uc}.out $benchmark_results_dir
 done
 
